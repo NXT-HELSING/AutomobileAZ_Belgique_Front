4f8a8a86f34306803b21c58255bcfa1f41219991
NXT-HELSING <pibo.heelsing@gmail.com>
2026-06-08 14:26:43 +0100

    feat: complete App.js with all routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+// Pages
+import Home from './pages/Home';
 import VehiclesList from './pages/VehiclesList';
 import VehicleDetail from './pages/VehicleDetail';
+import About from './pages/About';
+import Contect from './pages/Contect';
+import Error404 from './pages/Error404';
 
-// Import your template CSS (adjust paths as needed)
+// CSS imports
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'swiper/css/bundle';
+import 'aos/dist/aos.css';
+import './assets/css/style.css';
+import './assets/css/skin/skin-1.css';
 
 function App() {
+    useEffect(() => {
+        import('aos').then((AOS) => {
+            AOS.init({
+                duration: 1000,
+                once: true,
+            });
+        });
+    }, []);
+
     return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<VehiclesList />} />
-                    <Route path="/vehicle/:id" element={<VehicleDetail />} />
-                </Routes>
-            </div>
-        </Router>
+        <HelmetProvider>
+            <Router>
+                <div className="page-wraper">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/cars" element={<VehiclesList />} />
+                        <Route path="/vehicle/:id" element={<VehicleDetail />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contect />} />
+                        <Route path="*" element={<Error404 />} />
+                    </Routes>
+                </div>
+            </Router>
+        </HelmetProvider>
     );
 }
 
